d0430b47ecc48e0c96325d95a45355edb606b71c
yangming <472102644@qq.com>
2019-09-02 16:23:29 +0800

    fix: 新增esc-ui

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   presets: [
     '@vue/app'
+  ],
+  plugins: [
+    [
+      'import',
+      {
+        libraryName: 'esc-ui',
+        style: true
+      },
+      'esc-ui'
+    ]
   ]
 }
