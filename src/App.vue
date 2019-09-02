cdfb55b73e91094bcc7499cfee4ab3433f6f8a35
yangming <472102644@qq.com>
2019-09-02 11:06:40 +0800

    fix: template

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,42 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
+<script>
+export default {
+  mounted () {
+    window.addEventListener('message', (e) => {
+      switch (e.data) {
+        case 'reload':
+          location.reload(true)
+          break
+        default:
+          try {
+            const data = JSON.parse(e.data)
+            switch (data.type) {
+              case 'push':
+                this.$router.push(data.path)
+                break
+            }
+          } catch (e) {
+            // console.log(e)
+          }
+      }
+    })
+  }
+}
+</script>
+
 <style lang="less">
+html, body, #app {
+  height: 100%;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Http from 'esc-ui/lib/http'
+import 'tcon'
+import urlMap from './views-template'
 
 Vue.config.productionTip = false
+Vue.prototype.$http = new Http({
+  // baseUrl: 'http://app.e.uban360.net',
+  urlMap: {
+    ...(urlMap || {})
+  },
+  beforeThen (data, attaches) {
+    console.log(data, attaches)
+    return data
+  }
+})
 
 new Vue({
   router,
